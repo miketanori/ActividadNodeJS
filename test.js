cd7719a8b25a2c33af352602e13f384e846259ee
Mike Tanori <miguel.tanori@hotmail.com>
2022-05-03 19:39:00 -0500

    HTML

--- test.js
+++ test.js
@@ -1,1 +1,34 @@
 console.log('Instalación correcta, Hola!');
+// Paquete necesario para conectar a bases de datos MySQL.
+var mysql = require('mysql');
+// Consulta SQL.
+var sql = 'SELECT * FROM category LIMIT 10';
+
+// Parámetros de conexión a la base de datos.
+var con = mysql.createConnection({
+  host: "tu-host",
+  user: "tu-user-de-db",
+  password: "tu-pass-de-db",
+  database : 'tu-nombre-de-db'
+});
+
+// Funcion que nos permite comprobar la conexión a la base de datos.
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+// Funcion que nos devolverá resultados de la base de datos.
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    // Bucle que recore los resultados y pinta en consola.
+    for(i=0; i<result.length; i++){
+    	console.log("Result: " + result[i].name);
+    }
+
+  });
+});
